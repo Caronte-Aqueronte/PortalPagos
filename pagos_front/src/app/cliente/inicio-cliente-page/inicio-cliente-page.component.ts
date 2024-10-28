5381300c066248fefe8a6cd89ddfad710fad3c8f
Caronte-Aqueronte <elrincondelgamer77@gmail.com>
2024-10-28 06:28:48 -0600

    Terminada la integracion de recarga y retiro

--- pagos_front/src/app/cliente/inicio-cliente-page/inicio-cliente-page.component.ts
+++ pagos_front/src/app/cliente/inicio-cliente-page/inicio-cliente-page.component.ts
@@ -3,6 +3,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { TransaccionService } from '../../services/transaccion.service';
 import { AuthService } from '../../services/auth.service';
 import { SaldoService } from '../../services/saldo.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-inicio-cliente-page',
@@ -10,6 +11,10 @@ import { SaldoService } from '../../services/saldo.service';
   styleUrl: './inicio-cliente-page.component.css',
 })
 export class InicioClientePageComponent implements OnInit {
+  externoForm: FormGroup;
+  showDialog = false;
+  operationType = 'Recargar';
+
   saldoUsuario: any;
   transferForm: FormGroup; // Formulario reactivo
   showTransferPopup: boolean = false; // Variable para controlar la visibilidad del popup
@@ -21,7 +26,8 @@ export class InicioClientePageComponent implements OnInit {
     private fb: FormBuilder,
     private transaccionService: TransaccionService,
     private authService: AuthService,
-    private saldoService: SaldoService
+    private saldoService: SaldoService,
+    private toastr: ToastrService
   ) {
     // Inicializar el formulario reactivo de transferencia
     this.transferForm = this.fb.group({
@@ -35,6 +41,19 @@ export class InicioClientePageComponent implements OnInit {
         ],
       ], // Validación para email
       cantidad: ['', [Validators.required, Validators.min(1)]], // Validación para monto (mínimo 1)
+    });
+
+    this.externoForm = this.fb.group({
+      operationType: ['Recargar', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      pin: ['', [Validators.required, Validators.minLength(3)]],
+      monto: ['', [Validators.required, Validators.min(1)]],
+      banco: ['credito', Validators.required],
+    });
+
+    // Escucha cambios en el tipo de operación
+    this.externoForm.get('operationType')?.valueChanges.subscribe((value) => {
+      this.operationType = value;
     });
   }
   ngOnInit(): void {
@@ -131,11 +150,80 @@ export class InicioClientePageComponent implements OnInit {
     }
   }
 
+  // Envía el formulario
+  submitTransaction() {
+    if (this.externoForm.valid) {
+      const formData = this.externoForm.value;
+
+      if (formData.operationType == 'Recargar') {
+        this.recargar(formData);
+      } else if (formData.operationType == 'Retirar') {
+        this.retirar(formData);
+      } else {
+        this.toastr.error('Operacion invalida.', 'Error');
+      }
+
+      this.closeDialog();
+    } else {
+      this.toastr.error(
+        'Por favor completa el formulario correctamente.',
+        'Error'
+      );
+    }
+  }
+
+  recargar(body: any) {
+    // Llama al servicio de eliminación de cuenta, pasando la contraseña
+    this.saldoService.recargar(body).subscribe({
+      next: () => {
+        this.toastr.success(
+          `Recarga realizada exitosamente por Q${body.monto}`,
+          'Éxito'
+        );
+      },
+      error: (err) => {
+        // Manejo del error, muestra el mensaje adecuado
+        var mensajeError =
+          err.error || 'Ocurrió un error al intentar recargar.';
+        this.toastr.error(mensajeError, 'Error');
+      },
+    });
+  }
+
+  retirar(body: any) {
+    // Llama al servicio de eliminación de cuenta, pasando la contraseña
+    this.saldoService.retirar(body).subscribe({
+      next: () => {
+        this.toastr.success(
+          `Retiro realizado exitosamente por Q${body.monto}`,
+          'Éxito'
+        );
+      },
+      error: (err) => {
+        // Manejo del error, muestra el mensaje adecuado
+        var mensajeError = err.error || 'Ocurrió un error al intentar retirar.';
+        this.toastr.error(mensajeError, 'Error');
+      },
+    });
+  }
+
   closeErrorAlert() {
     this.errorMessage = null;
   }
 
   closeSuccessAlert() {
     this.successMessage = null;
+  }
+
+  // Función para abrir el diálogo con el tipo de operación específico
+  openDialog(operation: 'Recargar' | 'Retirar') {
+    this.operationType = operation;
+    this.externoForm.get('operationType')?.setValue(operation);
+    this.showDialog = true;
+  }
+
+  // Cierra el diálogo
+  closeDialog() {
+    this.showDialog = false;
   }
 }
--- pagos_front/src/app/services/saldo.service.ts
+++ pagos_front/src/app/services/saldo.service.ts
@@ -3,21 +3,29 @@ import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../enviorment';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SaldoService {
-
   private baseUrl = environment.apiUrl + '/saldo/'; // URL base de la API
   private apiPublic = environment.apiUrl + '/saldo/public'; // URL base de la API
   private apiProtected = environment.apiUrl + '/saldo/protected'; // URL base de la API
 
+  constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) { }
+  // Método para obtener la información del usuario por su ID
+  getMiSaldo(id: number): Observable<any> {
+    const url = `${this.apiProtected}/getMiSaldo/${id}`; // URL con el ID del usuario
+    return this.http.get(url); // Realiza la solicitud GET
+  }
 
-    // Método para obtener la información del usuario por su ID
-    getMiSaldo(id: number): Observable<any> {
-      const url = `${this.apiProtected}/getMiSaldo/${id}`; // URL con el ID del usuario
-      return this.http.get(url); // Realiza la solicitud GET
-    }
-  
+  // Método para obtener la información del usuario por su ID
+  retirar(body: any): Observable<any> {
+    const url = `${this.apiProtected}/retirar`; // URL con el ID del usuario
+    return this.http.patch(url, body); // Realiza la solicitud GET
+  }
+
+  recargar(body: any): Observable<any> {
+    const url = `${this.apiProtected}/recargar`; // URL con el ID del usuario
+    return this.http.patch(url, body); // Realiza la solicitud GET
+  }
 }
